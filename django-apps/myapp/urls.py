2554956daeb42c3f739fbe4aa389304ee151b666
Poong-hyerim <82091932+Poong-hyerim@users.noreply.github.com>
2022-06-23 16:23:42 +0900

    django_실습#1(templates 생성x)
    django_실습#1(templates 생성x)
    프로젝트 명 : myproject
    앱 명 : myapp

---
+++ django-apps/myapp/urls.py
@@ -0,0 +1,11 @@
+
+from turtle import update
+from django.urls import path
+from myapp import views
+urlpatterns = [
+    path('', views.index),
+    path('create/', views.create),
+    path('read/<id>/', views.read),
+    path('delete/', views.delete),
+    path('update/<id>/', views.update),
+]
